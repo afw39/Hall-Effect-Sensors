d2b71238fd98eff249ca43469b526098c7f5d499
afw39 <aoife.willis@ukaea.uk>
2026-09-16 12:34:45 +0100

    committing updated arduino code for 4 sensors

--- software/src/arduino_code/arduino_code.ino
+++ software/src/arduino_code/arduino_code.ino
@@ -1,49 +1,41 @@
-const float VCC = 5.00;
+
+const float VCC = 3.3;
 const int sensor1= A0;
 const int sensor2 = A1;
+const int sensor3 = A2;
+const int sensor4 = A3;
 
-int data1, data2;
+int data1, data2, data3, data4;
 
 String dataLabel1 = "sensor1";
 String dataLabel2 = "sensor2";
+String dataLabel3 = "sensor3";
+String dataLabel4 = "sensor4";
 
 void setup() {
   Serial.begin(9600);
   pinMode(sensor1, INPUT);
   pinMode(sensor2, INPUT);
+  pinMode(sensor3, INPUT);
+  pinMode(sensor4, INPUT);
+  Serial.println("time_ms, sensor1, sensor2, sensor3, sensor4");
 }
 
 void loop(){
-
-  // can add a like while millis < x for a timed sample intake, or can do a while samples < x as well if want to
-  
-  if(label){
-    Serial.print("time_ms");
-    Serial.print(",");
-    Serial.print(dataLabel1);
-    Serial.print(",");
-    Serial.println(dataLabel2);
-  }
-
+ 
   data1 = analogRead(sensor1);
   data2 = analogRead(sensor2);
-  // printing the data as reading_1, reading_2 under the dataLabel headings above
+  data3 = analogRead(sensor3);
+  data4 = analogRead(sensor4);
   Serial.print(millis());
   Serial.print(",");
   Serial.print(data1);
   Serial.print(",");
-  Serial.println(data2);
+  Serial.print(data2);
+  Serial.print(",");
+  Serial.print(data3);
+  Serial.print(",");
+  Serial.println(data4);
 
-  delay(500); // takes one reading every 500 ms
+  delay(100); 
 }
-
-// should work for as many sensors as i need, can print them all in a line separated by commas, read them into python
-// as a csv and then do it from there
-// will produce something like this
-// time_ms      sensor1     sensor2
-//       0       680          703    
-//     500       702          689
-//    1000       695          688
-//    1500       687          694
-//    2000       793          707
- 
