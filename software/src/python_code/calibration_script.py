983b084224772d7c2784dcdb0f732460bb59b228
afw39 <aoife.willis@ukaea.uk>
2026-09-17 16:30:59 +0100

    finished unfinished docstrings

--- software/src/python_code/calibration_script.py
+++ software/src/python_code/calibration_script.py
@@ -1,5 +1,5 @@
 from calibration import Calibration
 
-x = Calibration(number = 4, port = '/dev/ttyACM0', vcc = 3.3,
+x = Calibration(number = 1, port = '/dev/ttyACM0', vcc = 3.3,
                 filename = 'calibration-data.csv',samples = 50, delay = 1)
 x.perform_calibration(fields = [2, 6])
